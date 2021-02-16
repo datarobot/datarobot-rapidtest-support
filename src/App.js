a08963650a0bcffe339d6ed591c61c0694060c39
Craig Wermert <craig.wermert@datarobot.com>
2021-02-16 13:22:46 -0500

    landing page

--- src/App.js
+++ src/App.js
@@ -1,30 +1,22 @@
-import React, { Suspense, lazy } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React, { Suspense } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'jotai';
 
 import Header from 'components/Header';
 import Footer from 'components/Footer';
 
-import './App.css';
+import Routes from 'Routes';
 
-const Home = lazy(() => import('views/Home'));
-const Sites = lazy(() => import('views/Sites'));
-const Accounts = lazy(() => import('views/Accounts'));
+import './App.css';
 
 const App = () => (
   <Provider>
     <Router>
       <div className="App">
         <Header />
-
         <Suspense fallback={<div>Loading</div>}>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/sites" component={Sites} />
-            <Route path="/accounts" component={Accounts} />
-          </Switch>
+          <Routes />
         </Suspense>
-
         <Footer />
       </div>
     </Router>
--- src/Routes.js
+++ src/Routes.js
@@ -2,11 +2,17 @@ import { lazy } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 const Home = lazy(() => import('views/Home'));
+const ProgramAdmin = lazy(() => import('views/ProgramAdmin'));
+const Sites = lazy(() => import('views/Sites'));
+const Accounts = lazy(() => import('views/Accounts'));
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route exact path="/program-admin" component={ProgramAdmin} />
+      <Route path="/sites" component={Sites} />
+      <Route path="/accounts" component={Accounts} />
     </Switch>
   </BrowserRouter>
 );
