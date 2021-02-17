ef04ad5b01c062d0d083fe5c07550ecfdd78800c
Craig Wermert <craig.wermert@datarobot.com>
2021-02-17 09:59:54 -0500

    new landing page

--- src/Routes.js
+++ src/Routes.js
@@ -1,20 +1,20 @@
 import { lazy } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 const Home = lazy(() => import('views/Home'));
 const ProgramAdmin = lazy(() => import('views/ProgramAdmin'));
+const Other = lazy(() => import('views/Other'));
 const Sites = lazy(() => import('views/Sites'));
 const Accounts = lazy(() => import('views/Accounts'));
 
 const Routes = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/program-admin" component={ProgramAdmin} />
-      <Route path="/sites" component={Sites} />
-      <Route path="/accounts" component={Accounts} />
-    </Switch>
-  </BrowserRouter>
+  <Switch>
+    <Route path="/program-admin" component={ProgramAdmin} />
+    <Route path="/other" component={Other} />
+    <Route path="/sites" component={Sites} />
+    <Route path="/accounts" component={Accounts} />
+    <Route path="/" component={Home} />
+  </Switch>
 );
 
 export default Routes;
