809b869d4fd835b0301bafb45379c442f9ce1c5e
Craig Wermert <craig.wermert@datarobot.com>
2021-02-24 10:05:55 -0500

    select stuff

--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -1,45 +1,53 @@
 // @ts-nocheck
+import { forwardRef } from 'react';
 import cls from 'classnames';
+
 import './Select.css';
 
-const Select = ({
-  options = [],
-  label,
-  isRequired,
-  name,
-  value,
-  className,
-  onChange,
-  placeholder,
-  ...rest
-}) => (
-  <>
-    {label && (
-      <label
-        className={cls('select-label', { required: isRequired })}
-        htmlFor={name}
-      >
-        {label}
-      </label>
-    )}
+const Select = forwardRef(
+  (
+    {
+      options = [],
+      label,
+      isRequired,
+      name,
+      value,
+      className,
+      onChange,
+      placeholder,
+      ...rest
+    },
+    ref
+  ) => (
+    <>
+      {label && (
+        <label
+          className={cls('select-label', { required: isRequired })}
+          htmlFor={name}
+        >
+          {label}
+        </label>
+      )}
 
-    <select
-      onChange={onChange}
-      id={name}
-      className={cls(className, 'select')}
-      value={value}
-      {...rest}
-    >
-      <option value="DEFAULT" disabled>
-        {placeholder}
-      </option>
-      {options.map((option) => (
-        <option key={option.value} value={option.value}>
-          {option.label}
+      <select
+        ref={ref}
+        onChange={onChange}
+        id={name}
+        className={cls(className, 'select')}
+        value={value || 'DEFAULT'}
+        {...rest}
+      >
+        <option value="DEFAULT" disabled>
+          {placeholder}
         </option>
-      ))}
-    </select>
-  </>
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    </>
+  )
 );
 
 export default Select;
