e0f5503203f6909309964ef13c3d48ee57024b85
Craig Wermert <craig.wermert@datarobot.com>
2021-04-13 09:34:44 -0400

    loading indicator

--- src/components/Table2/AccountRenderers/AccountStatusCell.js
+++ src/components/Table2/AccountRenderers/AccountStatusCell.js
@@ -1,8 +1,18 @@
+import { useState } from 'react';
+import cls from 'classnames';
 import { editAccount } from 'services/api';
 
+import Loading from 'components/Loading';
+
 const AccountStatusCell = ({ data }) => {
+  const [isLoading, setIsLoading] = useState(false);
+
   const resendEmail = () => {
-    editAccount(data.id, { resend_email: true });
+    setIsLoading(true);
+
+    editAccount(data.id, { resend_email: true }).then(() =>
+      setIsLoading(false)
+    );
   };
 
   if (data.archive) {
@@ -10,14 +20,23 @@ const AccountStatusCell = ({ data }) => {
   }
   if (!data.last_login_ip) {
     return (
-      <span className="flex justify-end">
-        <button
-          onClick={resendEmail}
-          className="btn-clear font-normal text-base py-1 px-4 text-orange text-right focus:outline-none"
-          type="button"
-        >
-          Resend Email
-        </button>
+      <span
+        className={cls('flex', {
+          'justify-end': !isLoading,
+          'justify-center': isLoading,
+        })}
+      >
+        {isLoading ? (
+          <Loading containerClassName="w-8" size={32} />
+        ) : (
+          <button
+            onClick={resendEmail}
+            className="btn-clear font-normal text-base py-1 px-4 text-orange text-right focus:outline-none"
+            type="button"
+          >
+            Resend Email
+          </button>
+        )}
       </span>
     );
   }
