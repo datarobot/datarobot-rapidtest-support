5afad8f3933884bbf525e721f4213a00e6011a90
Yuriy Hrytsyuk <yuriy.hrytsyuk@datarobot.com>
2021-05-24 18:41:56 +0300

    Cleanup

--- src/components/TableAdvancedV2/AccountRenderers/AccountNameCell.js
+++
@@ -1,7 +0,0 @@
-import Highlight from 'components/Highlight';
-
-const AccountNameCell = ({ data }) => {
-  return <Highlight text={`${data.last_name}, ${data.first_name}`} />;
-};
-
-export default AccountNameCell;
