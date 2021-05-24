5337028421eb01bae9e490cda4edcebafaf5e880
Yuriy Hrytsyuk <yuriy.hrytsyuk@datarobot.com>
2021-05-24 14:48:04 +0300

    Fix pagination

--- src/components/TableAdvanced/Pagination.js
+++ src/components/TableAdvanced/Pagination.js
@@ -209,7 +209,7 @@ const Pagination = ({
         </span>
       )}
 
-      <div className="flex items-center justify-center">
+      <div className="pagination">
         <button
           className="paging-button"
           onClick={() => onBtnFirst()}
